864983e2da1bcdc7f7cec1bbb4d83a1186a4cb74
junmindereal <junmindereal@gmail.com>
2020-03-13 11:59:48 +0800

    clean up

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
-const purgecss = require('@fullhuman/postcss-purgecss')({
+const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ['./src/**/*.js', './src/**/*.jsx', './public/index.html'],
+  content: ["./src/**/*.js", "./src/**/*.jsx", "./public/index.html"],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
@@ -9,8 +9,8 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
-    <div>
+    <div className="bg-green-1000">
       <h1>Hello World</h1>
       <img src={logo} alt="logo" />
     </div>
