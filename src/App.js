4368e309b9666ebe2ba85de4c93c33a181cd046c
junmindereal <junmindereal@gmail.com>
2020-04-21 16:42:56 +0800

    Added static testing config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import Header from "./components/Header";
+import React from 'react';
+import Header from './components/Header';
 
 function App() {
   const styles = {
-    container: "container xl mx-auto",
+    container: 'container xl mx-auto',
   };
 
   return (
